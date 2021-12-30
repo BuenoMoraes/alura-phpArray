2d2f91e19995166a2e50378f4b38627087b7549c
Matheus Bueno <bueno_matheus@outlook.com>
2021-12-30 12:26:17 -0300

    Inicio aulas Array

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$notas = array(9, 3, 10, 5, 10);
+
+//$notas = [9, 3, 10, 5, 10];
+
+echo "<p>Nota de português: $notas[0]</p>";
+echo "<p>Nota de matemática: $notas[1]</p>";
+echo "<p>Nota de geografia: $notas[2]</p>";
+echo "<p>Nota de história: $notas[3]</p>";
+echo "<p>Nota de química: $notas[4]</p>";
+
+$quantidadeNotas = sizeof($notas);
