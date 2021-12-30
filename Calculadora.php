531d8773d874bd4c98d65172259dc23f32f604e0
Matheus Bueno <bueno_matheus@outlook.com>
2021-12-30 15:46:22 -0300

    Alterações aula 1

--- Calculadora.php
+++ Calculadora.php
@@ -1,5 +1,29 @@
 <?php
 
+class Calculadora
+{
+    public function calculaMedia(array $notas): ?float
+    {
+        $quantidadeNotas = sizeof($notas);
+        if ($quantidadeNotas !== 0) {
+            $soma = 0;
+
+            for ($i = 0; $i < $quantidadeNotas; $i++) {
+                $soma = $soma + $notas[$i];
+            }
+
+            $media = $soma / $quantidadeNotas;
+
+            return $media;
+        } else {
+            return null;
+        }
+    }
+}
+
+/*Index.php para utiização desta classe
+<?php
+
 require 'Calculadora.php';
 ;
 
