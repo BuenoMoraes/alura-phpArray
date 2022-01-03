22dedd02c2c85ad1beff283f362d89cb0cd9f5f0
Matheus Bueno <bueno_matheus@outlook.com>
2022-01-03 08:27:36 -0300

    Alerações aula2

--- index.php
+++ index.php
@@ -1,48 +1,18 @@
-<?php
+(<?php
 
-require 'Calculadora.php';
-;
+$saldos = [
+    2500,
+    3000,
+    4400,
+    1000,
+    8700,
+    9000
+];
 
-$notas = [9, 5, 10, 8];
-
-$calculadora = new Calculadora();
-$media = $calculadora->calculaMedia($notas);
-
-if ($media) {
-    echo "A média é: $media";
-} else {
-    echo "Não foi possível calcular a média";
+foreach ($saldos as $saldo) {
+    echo "<p>O saldo é $saldo</p>";
 }
 
-//$notas = [9, 3, 10, 5, 10];
-/*
-echo "<p>Nota de português: $notas[0]</p>";
-echo "<p>Nota de matemática: $notas[1]</p>";
-echo "<p>Nota de geografia: $notas[2]</p>";
-echo "<p>Nota de história: $notas[3]</p>";
-echo "<p>Nota de química: $notas[4]</p>";
+sort($saldos);
 
-$quantidadeNotas = sizeof($notas);
-
-$soma = 0;
-for ($i = 0; $i < $quantidadeNotas; $i++) {
-    $soma = $soma + $notas[$i];
-}
-
-$media = $soma / $quantidadeNotas;
-
-if ($quantidadeNotas !== 0) {
-
-    $quantidadeNotas = sizeof($notas);
-
-    $soma = 0;
-    for ($i = 0; $i < $quantidadeNotas; $i++) {
-        $soma = $soma + $notas[$i];
-    }
-
-    $media = $soma / $quantidadeNotas;
-
-    echo $media;
-}
-
-echo "<p>A média é $media</p>";*/
+echo"O menor saldo é $saldos[0]";
